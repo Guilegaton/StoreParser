05705812443e322653e3930131c8290550cc67b4
Julius <yulii.vedkal@nure.ua>
2020-01-23 17:45:00 +0200

    [add] - add custom parsing exceptions

---
+++ StoreParser/HtmlParser/Exceptions/NotPropertyException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HtmlParser.Exceptions
+{
+    public class NotPropertyException : Exception
+    {
+        private static readonly string _errorMessage = "One member in template invocation chain is not a property of \"{0}\" class. Please check your HTML template";
+
+        public NotPropertyException(Type modelType) : base(string.Format(_errorMessage, modelType.ToString()))
+        {
+        }
+
+        public NotPropertyException(Type modelType, Exception innerException) : base(string.Format(_errorMessage, modelType.ToString()), innerException)
+        {
+        }
+    }
+}
---
+++ StoreParser/HtmlParser/Exceptions/PropertyNotFoundException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HtmlParser.Exceptions
+{
+    public class PropertyNotFoundException : Exception
+    {
+        private static readonly string _errorMessage = "Property \"{0}\" was not found into model of \"{1}\" type. Please, check your HTML template.";
+
+        public PropertyNotFoundException(string propertyName, Type modelType) : base(string.Format(_errorMessage, propertyName, modelType.ToString()))
+        {
+        }
+
+        public PropertyNotFoundException(string propertyName, Type modelType, Exception innerException) : base(string.Format(_errorMessage, propertyName, modelType.ToString()), innerException)
+        {
+        }
+
+    }
+}
